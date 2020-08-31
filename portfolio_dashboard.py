8e8a1b6fd9f1812dee0411c318465db00fc6e501
Jakub <jakubmoz003@gmail.com>
2020-08-31 16:53:33 -0500

    Testing Dashboard Functioanlity

--- portfolio_dashboard.py
+++ portfolio_dashboard.py
@@ -5,3 +5,25 @@
 
     # User can choose how much they want to invest 
     # user can view their watchlist of stocks as wellS
+
+def option():
+    print("""
+    You can choose 'STOCK' or 'NO'
+    """)
+
+option()
+while True:
+    new_item = input("PICK OPTION >  ")
+
+    if new_item == 'STOCK':
+        import stock_screener
+    
+    elif new_item == 'NO':
+        break
+    break
+
+print ("""
+
+FIRE DASHBOARD IS NOW CLOSED
+
+""")
