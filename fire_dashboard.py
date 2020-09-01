c6f269dafe7e71e8a0594c2964f8bd98ef4e0f3b
Jakub <jakubmoz003@gmail.com>
2020-08-31 22:36:52 -0500

    updated main dashboard settings

--- fire_dashboard.py
+++ fire_dashboard.py
@@ -5,5 +5,92 @@
         # stock_screener.py for an automated script to filter stocks
         # portfolio_dashboard.py to view the perfomrance of your stocks
             # You need to set up a user_template_settings.py file first before this will work
+import pandas as pd
+from pandas import DataFrame
+import time
+import random
+import numpy as np
+import math
+import datetime as dt
+import requests
+import os
+import json
+from tabulate import tabulate
+
+menu_items = [
+['STOCKS','> Stock Screener for flexible stock analysis'],
+['SIMPLEX','> Expected Value Analysis for Single Stock'],
+['CALC','> Launch Compound Interest Calculator'],
+['EFC','> Launch Effeceint Forntier Calculator'],
+['TEST','> Launch Strategy Backtest Calculator'],
+['SETTINGS','> Settings for Portfolio SetUp '],
+['PORTFOLIO','> View your portfolio or a mockup'],
+['SAVE','> Save your results as CSV'],
+['HELP','> View all the menu items again'],
+['EXIT','> End the program and current instance'],
+]
 
 
+def help_settings():
+    df = DataFrame(menu_items,  columns =['COMMANDS > ','DESCRIPTION'])
+    print(tabulate(df, showindex=False, headers=df.columns))
+
+
+print("""
+//////////////////////////
+///// FIRE DASHBOARD /////
+//////////////////////////
+
+.....MAIN MENU OPTIONS.....
+""")
+
+help_settings()
+
+while True:
+    print("")
+    new_item = input("PICK COMMAND >  ").upper()
+
+    if new_item == 'STOCK':
+        import stock_screener
+    
+    elif new_item == 'SIMPLEX':
+        print("This option is in development")
+        continue
+
+    elif new_item == 'CALC':
+        print("This option is in development")
+        continue
+
+    elif new_item == 'EFC':
+        print("This option is in development")
+        continue
+
+    elif new_item == 'TEST':
+        print("This option is in development")
+        continue
+
+    elif new_item == 'SETTINGS':
+        print("This option is in development")
+        continue
+    elif new_item == 'PORTFOLIO':
+        print("This option is in development")
+        continue
+
+    elif new_item == 'SAVE':
+        print("This option is in development")
+        continue
+
+    elif new_item == 'HELP':
+        help_settings()
+        continue
+
+    elif new_item == 'EXIT':
+        break
+
+    continue
+
+print ("""
+////////////////////////////
+FIRE DASHBOARD IS NOW CLOSED
+////////////////////////////
+""")
