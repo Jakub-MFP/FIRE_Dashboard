bed5e5d1c9419eccd5b27da8b4341e33deb6ad7e
rivaandreasanzell <69712004+rivaandreasanzell@users.noreply.github.com>
2020-09-25 15:37:45 +0800

    Create unit_conversion_tool.php

---
+++ Pet Projects/unit_conversion_tool.php
@@ -0,0 +1,32 @@
+<?php
+//number in pounds that we want to convert to kilograms
+$pounds = 140;
+// floating point value for the pound to kilogram conversion
+$lb_to_kg = 0.453592
+// use the variables above to calculate pounds multiplied by the kilogram conversion
+$kilogram = $pounds * $lb_to_kg;
+// display the pounds to kilograms
+echo "Weight: ";
+echo $pounds;
+echo " lb = ";
+echo $kilograms;
+echo " kg";
+
+// answer should be Weight: 140 lb = 63.50288 kg
+
+
+// number in miles we want to convert to kilometers
+$miles = 2.5;
+// floating point value for the mile to kilometer conversion
+$miles_to_km = 1.60934;
+// use the variable above to calculate miles multiplied by the kilometer conversion
+$kilometers = $miles * $mile_to_km;
+// display the miles to kilometers
+echo "Distance: ";
+echo $miles;
+echo " miles = ";
+echo $kilometers;
+echo " km";
+
+// answer should be Distance: 2.5 miles = 4.0333 5km
+?>
