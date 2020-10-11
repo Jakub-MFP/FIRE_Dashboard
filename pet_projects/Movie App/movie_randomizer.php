de462c821f3854f56b6cb76a56500033aec36efe
rivaandreasanzell <69712004+rivaandreasanzell@users.noreply.github.com>
2020-10-12 06:20:35 +0800

    movie watchlist

---
+++ pet_projects/Movie App/movie_randomizer.php
@@ -0,0 +1,7 @@
+<?php
+
+$my_movielist = array("TRAINWRECK","HAPPY GILMORE","21 JUMP STREET","GOOD MORNING, VIETNAM","HAROLD & KUMAR GO TO WHITE CASTLE");
+
+shuffle($my_movielist);
+print_r($my_movielist);
+?>
